b72f833f138559091354ae9aa2bf36057bb71dc3
Hobb211 <69100194+Hobb211@users.noreply.github.com>
2023-05-28 01:06:03 -0400

    feat(login): add logo image and login structure

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,21 +1,72 @@
 "use client"; // This is a client component 👈🏽
-import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import React, { useState, useEffect } from 'react';
+
 
 function page() {
+  const router = useRouter;
 
-    const [text, setText] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validEmail, setValidEmail] = useState(true);
+  const [validPassword, setValidPassword] = useState(true);
 
-    const handleChange = (event) => {
-      setText(event.target.value);
-    };
-  
-    return (
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = async (event) =>{
+    const userExists = await checkUser(email);
+    const isPasswordCorrect = await checkPassword(email,password);
+    if (validEmail && validPassword){
+      //inicio de sesión
+      router.push('/menu');
+    }
+  };
+
+  const checkUser = async (email)=>{
+
+  };
+
+  const checkPassword = async (email,password)=>{
+    if(password==="t123"){
+      setValidPassword(false);
+    }else{
+      setValidPassword(true);
+    }
+  };
+
+  useEffect(()=>{
+    console.log('Email:',email);
+    console.log('Password',password);
+    console.log("Valid Email:",validEmail);
+    console.log("Valid Password",validPassword);
+    if (validEmail && validPassword){
+      //inicio de sesión
+    }
+  },[validEmail,validPassword]);
+
+  return (
     <div>
+      <img src="/images/logo.png" alt="Logo de la aplicación web" />
         <h1>Inicia Sesión</h1>
-        <input type="text" value={text} onChange={handleChange} />
-        <p>Texto ingresado: {text}</p>
+        <div>
+          <label htmlFor="email">Correo:</label>
+          <input type="email" id="email" value={email} onChange={handleEmailChange} required/>
+          {!validEmail && <p className='error-message'>El correo no existe</p>}
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input type="password" id='password' value={password} onChange={handlePasswordChange} required/>
+          {!validPassword && <p className='error-message'>La contraseña es incorrecta</p>}
+        </div>
+        <button onClick={handleLogin}>Iniciar sesión</button>
     </div>
-  )
+  );
 }
 
 export default page
