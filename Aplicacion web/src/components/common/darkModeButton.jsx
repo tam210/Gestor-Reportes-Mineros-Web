7a5a214b8c76e1a767dcb294a56189624cc40eff
Hobb211 <69100194+Hobb211@users.noreply.github.com>
2023-06-02 22:22:15 -0400

    style(menu): add top bar and style menu

--- Aplicacion web/src/components/common/darkModeButton.jsx
+++ Aplicacion web/src/components/common/darkModeButton.jsx
@@ -1,23 +1,5 @@
 
 function DarkModeButton() {
-    if(localStorage.getItem('configuracion')===null){
-        const configuracion = {
-          tema: 'white'
-        };
-        localStorage.setItem('configuracion',JSON.stringify(configuracion));
-    }else{
-        const configuracion = JSON.parse(localStorage.getItem('configuracion'));
-        if(document.documentElement.classList.contains('dark')){
-          if(configuracion.tema === 'white'){
-            document.documentElement.classList.toggle('dark')
-          }
-        }else{
-          if(configuracion.tema === 'dark'){
-            document.documentElement.classList.toggle('dark')
-          }
-        }
-    }
-
     const handleClick = ()=>{
         document.documentElement.classList.toggle('dark')
         const configuracion = JSON.parse(localStorage.getItem('configuracion'));
--- Aplicacion web/src/components/menu.tsx
+++ Aplicacion web/src/components/menu.tsx
@@ -2,35 +2,32 @@
 import { useRouter } from "next/navigation"
 import DarkModeButton from '@/components/common/darkModeButton';
 import ThreeDots from "./common/ThreeDots";
-import React from "react";
+import React, {useEffect} from "react";
+import TopBar from "./common/topBar";
 
 function Menu() {
     const router=useRouter();
-    // const token ={'id':'algo','rol':'admin'};
-    const token = localStorage.getItem('token');
-
-    /* if(token === null){
-        router.push('/');
-    } */
+    let token ='algo';
+    const token1={'rol':'admin'}
     
     const toReports = () => {
         router.push('/reports');
     }
 
     const toUsers = () => {
-        if(token.rol === 'admin'){
+        if(token1.rol === 'admin'){
             router.push('/admin/users');
         };
     }
 
     const toRequest = () => {
-        if(token.rol === 'admin'){
+        if(token1.rol === 'admin'){
             router.push('/admin/request');
         };
     }
 
     const toFC = () => {
-        if(token.rol==='admin'){
+        if(token1.rol==='admin'){
             router.push('/admin/fc');
         };
     }
@@ -40,18 +37,36 @@ function Menu() {
         router.push('/')
     }
 
+    useEffect(()=>{
+        token = localStorage.getItem('token');
+        /* if(token === null){
+            router.push('/');
+        } 
+        
+        */
+    },[])
+
     return (
         <div className="dark:bg-black">
             {
                 token ?
                 (
-                    <div className="container mx-auto min-h-screen text-center">
-                        <div className="">
-                            <DarkModeButton/>
-                            <button className="button" onClick={toReports}>Reportes</button>
-                            <button className="button" onClick={toUsers}>Usuarios</button>
-                            <button className="button" onClick={toRequest}>Solicitudes de acceso</button>
-                            <button className="button" onClick={toFC}>Factor de carga</button>
+                    <div className="mx-auto min-h-screen text-center">
+                        <div className="grid items-center grid-rows-6 min-h-screen">
+                            <TopBar message='Menu'/>
+                            <div className="flex flex-col row-span-4 max-w-md mx-auto bg-gray-150 shadow-slate-600 rounded-xl shadow-md overflow-hidden md:w-5/6 md:max-w-2xl dark:bg-gray-500 dark:shadow-sky-400 dark:shadow-lg">
+                                <div className="grid grid-cols-2 md:flex">
+                                    <div className="md:shrink-0">
+                                        <img src="/images/minera.jpg" className="h-full w-full object-cover md:w-64"/>
+                                    </div>
+                                    <div className="grid grid-rows-4 items-center mx-auto p-8"> 
+                                        <button className="button" onClick={toReports}>Reportes</button>
+                                        <button className="button" onClick={toUsers}>Usuarios</button>
+                                        <button className="button" onClick={toRequest}>Solicitudes de acceso</button>
+                                        <button className="button" onClick={toFC}>Factor de carga</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 ) : (
