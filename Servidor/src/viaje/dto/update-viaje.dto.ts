f709eebe501c7adee220b4082b8084e3f194dd2e
tam210 <ossandon.t20@gmail.com>
2023-07-18 00:00:23 -0400

    Findall zona y flota

--- Servidor/src/viaje/dto/update-viaje.dto.ts
+++ Servidor/src/viaje/dto/update-viaje.dto.ts
@@ -2,11 +2,20 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateViajeDto } from './create-viaje.dto';
 
 export interface UpdateViajeDto {
-    idviaje: number,
-    fecha?: number,
-    idorigen: number,
-    idflota: number,
-    tonelaje: number
+    // idviaje: number,
+    // idorigen: number,
+    // idflota: number,
+    // tonelaje: number,
+    // fechaInicio: Date,
+    // fechaFin: Date,
+    // //idzona y idflota
+    idorigen: number;
+
+    idflota: number;
+    idzona?: number;  // nueva propiedad
+    tonelaje: number;
+    fechaInicio: Date;  
+    fechaFin: Date;
 }
 
 
