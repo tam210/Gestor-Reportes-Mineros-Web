5b446629aa3ebae20e03544c319b06f19f6cb810
Hobb211 <69100194+Hobb211@users.noreply.github.com>
2023-07-18 02:16:18 -0400

    WebApp(chargeFactor): añadida funcionalidad factor de carga

--- Aplicacion web/src/components/chargeFactor.tsx
+++ Aplicacion web/src/components/chargeFactor.tsx
@@ -18,15 +18,40 @@ const handleFlota = async ()=>{
     return response.data
 }
 
+const handleZona = async ()=>{
+    const ENDPOINT = 'http://localhost:3001/zona'
+    const config = {
+        headers:{
+            Authorization: 'Bearer '+localStorage.getItem('token')
+        }
+    }
+    const response = await axios.get(ENDPOINT,config)
+    return response.data
+}
+
+const sendRequest = async (request)=>{
+    const ENDPOINT = ''
+    const config = {
+        headers:{
+            Authorization: 'Bearer '+localStorage.getItem('token')
+        }
+    }
+    const response = await axios.patch(ENDPOINT,request,config)
+    return response.data
+}
+
 function ChargeFactor() {
     const [startedDate, setStartedDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
-    const [chargeFactor, setChargeFactor] = useState(null);
+    const [chargeFactor, setChargeFactor] = useState('');
     const [flota, setFlota]=useState([])
+    const [selectFlota, setSelectFlota]=useState('')
+    const [zona, setZona]=useState([])
+    const [selectZona, setSelectZona]=useState('')
     const [validUser,setValidUser]= useState(true);
+    const [validRequest,setValidRequest]=useState(true)
     const router = useRouter()
     let token = localStorage.getItem('token')
-    console.log(token)
     const handleStartChange = (date) => {
         setStartedDate(date);
     };
@@ -38,25 +63,65 @@ function ChargeFactor() {
         if(value===''||/^\d+$/.test(value)){
             setChargeFactor(event.target.value);
         }
+        if(selectFlota===''){
+            setSelectFlota(flota.find(item=>item.idflota===0).nombre)
+        }
+        if(selectZona===''){
+            setSelectZona(zona.find(item=>item.idzona===0).nombre)
+        }
     }
+
+    const obtenerFechaFormateada = fecha => {
+        const options = { year: 'numeric' , month: '2-digit', day: '2-digit'};
+        return fecha.toLocaleDateString('en-US', options);
+      };
         
     const validToken =( async ()=>{
         token = localStorage.getItem('token');
-        console.log(token)
         if(token === null){
             router.push('/auth/login');
         } 
         try {
             await handleFlota().then((row)=>setFlota(row))
+            await handleZona().then((row)=>setZona(row))
         } catch (error) {
             localStorage.removeItem('token');
             setValidUser(false)
         }
     })
 
+    const changeFlota = (event)=>{
+        setSelectFlota(event.target.value)
+    }
+
+    const changeZona = (event)=>{
+        setSelectZona(event.target.value)
+    }
+
+    const confirm = ()=>{
+        try {
+            if(chargeFactor!==''){
+                const request={
+                    'idflota':(flota.find(item=>item.nombre===selectFlota)).idflota,
+                    'idzona':(zona.find(item=>item.nombre===selectZona)).idzona,
+                    'fechaInicio':obtenerFechaFormateada(startedDate),
+                    'fechaFin':obtenerFechaFormateada(endDate),
+                    'tonelaje':chargeFactor
+                }
+                setValidRequest(true)
+                sendRequest(request)
+            }else{
+                setValidRequest(false)
+            }
+        } catch (error) {
+            setValidRequest(false)
+        }
+        
+    }
+
     useEffect(()=>{
         validToken()
-    })
+    },[])
     
   return (
     <div className='flex flex-col h-screen w-full dark:bg-black'>
@@ -77,36 +142,36 @@ function ChargeFactor() {
                                 <div className='sm:mx-4'>
                                     <label className="flex my-3">
                                         <span>Flota:</span>
-                                        <select className='mx-3 w-full' name="fleet" id="">
+                                        <select className='mx-3 w-full text-black' name="flota" onChange={changeFlota}>
                                             {flota.map((option) => (
-                                                <option key={option.value} value={option.value}>
-                                                {option.label}
+                                                <option key={option.idflota} value={option.nombre}>
+                                                {option.nombre}
                                                 </option>
                                             ))} usar el codigo anterior para recibir un arreglo de flotas llamado options y crear una opcion para cada flota
                                         </select>
                                     </label>
                                     <label className="flex my-3">
                                         <span>Fase:</span>
-                                        <select className='mx-3 w-full' name="fleet" id="">
-                                            {/* {options.map((option) => (
-                                                <option key={option.value} value={option.value}>
-                                                {option.label}
+                                        <select className='mx-3 w-full text-black' name="zona" onChange={changeZona}>
+                                            {zona.map((option) => (
+                                                <option key={option.idzona} value={option.nombre}>
+                                                {option.nombre}
                                                 </option>
-                                            ))} usar el codigo anterior para recibir un arreglo de flotas llamado options y crear una opcion para cada flota */}
+                                            ))} usar el codigo anterior para recibir un arreglo de flotas llamado options y crear una opcion para cada flota
                                         </select>
                                     </label>
                                 </div>
                                 <div className='sm:ml-4'>
                                     <label className="block my-3">
                                         <span>Fecha de inicio:</span>
-                                        <DatePicker className='mx-3' selected={startedDate}
+                                        <DatePicker className='mx-3 text-black' selected={startedDate}
                                                     onChange={handleStartChange}
                                                     dateFormat="dd/MM/yyyy"
                                                     placeholderText="Selecciona una fecha"/>
                                     </label>
                                     <label className="block my-3">
                                         <span>Fecha de termino:</span>
-                                        <DatePicker className='mx-3' selected={endDate}
+                                        <DatePicker className='mx-3 text-black' selected={endDate}
                                                     onChange={handleEndChange}
                                                     dateFormat="dd/MM/yyyy"
                                                     placeholderText="Selecciona una fecha"/>
@@ -117,11 +182,12 @@ function ChargeFactor() {
                                 <div>
                                     <label className="block my-3">
                                         <span>Factor de carga:</span>
-                                        <input className='mx-3' type="number" id='chargeFactor'  value={chargeFactor} onChange={handleChargeFactor} placeholder='Ingrese el factor de carga'/>
+                                        <input className='mx-3 text-black' type="number" id='chargeFactor'  value={chargeFactor} onChange={handleChargeFactor} placeholder='Ingrese el factor de carga'/>
                                     </label>
                                 </div>
                                 <div className='self-center'>
-                                <button className='button w-36'>Confirmar</button>
+                                <button className='button w-36' onClick={confirm}>Confirmar</button>
+                                {!validRequest && <p className='text-red-600'>Completa todos los campos</p>}
                                 </div>
                             </div>
                         </div>
